f2913b898e3273ac38a7002e53235dc458a2cb18
Malibu <malibu090@mail.ru>
2022-11-28 04:20:11 +0300

    Example002 Hello user

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя");
-string username = Console.read();
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);
